8ddc9476f890ef2a2ff009e7db53258682d2a2c3
Shiwei Chen <shiweichenus@gmail.com>
2022-06-25 17:52:35 -0500

    Set up RelatedProduct class component

---
+++ client/src/components/RelatedProduct/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class RelatedProduct extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+    <div>
+      This is Related Product!
+    </div>
+    )
+  }
+}
+
+export default RelatedProduct;
+

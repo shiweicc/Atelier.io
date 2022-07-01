a4db2205194a6c0071015264a33afbd4111d0759
Jackson Armstrong <jackson@Jacksons-MacBook-Pro.local>
2022-07-01 13:49:31 -0700

    Added more component files and added state to index.js

--- client/src/components/RnR/index.jsx
+++ client/src/components/RnR/index.jsx
@@ -4,14 +4,77 @@ class RnR extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      product: "2",
+      page: 0,
+      count: 5,
+      results: [
+        {
+          "review_id": 5,
+          "rating": 3,
+          "summary": "I'm enjoying wearing these shades",
+          "recommend": false,
+          "response": null,
+          "body": "Comfortable and practical.",
+          "date": "2019-04-14T00:00:00.000Z",
+          "reviewer_name": "shortandsweeet",
+          "helpfulness": 5,
+          "photos": [{
+              "id": 1,
+              "url": "urlplaceholder/review_5_photo_number_1.jpg"
+            },
+            {
+              "id": 2,
+              "url": "urlplaceholder/review_5_photo_number_2.jpg"
+            }
+          ]
+        },
+        {
+          "review_id": 3,
+          "rating": 4,
+          "summary": "I am liking these glasses",
+          "recommend": false,
+          "response": "Glad you're enjoying the product!",
+          "body": "They are very dark. But that's good because I'm in very sunny spots",
+          "date": "2019-06-23T00:00:00.000Z",
+          "reviewer_name": "bigbrotherbenjamin",
+          "helpfulness": 5,
+          "photos": [],
+        }
+      ],
+      ratings: {
+        2: 1,
+        3: 1,
+        4: 2
+      },
+      recommended: {
+        0: 5
+      },
+      characteristics: {
+        "Size": {
+          "id": 14,
+          "value": "4.0000"
+        },
+        "Width": {
+          "id": 15,
+          "value": "3.5000"
+        },
+        "Comfort": {
+          "id": 16,
+          "value": "4.0000"
+        }
+      },
+      sortOrder: 'Relevance',
+      ratingsAvg: 3.5
     }
   }
 
   render() {
     return (
     <div>
-      This is Ratings and Reviews!
+      This is the index file!
+      <ReviewList results={this.state.results} sortOrder={this.state.sortOrder}/>
+      <RatingsBreakdown ratings={this.state.ratings} recommended={this.state.recommended}/>
+      <CharacteristicsBreakdown characteristics={this.state.characteristics} />
     </div>
     )
   }
--- client/src/components/RnR/rnr.spec.js
+++ client/src/components/RnR/rnr.spec.js
@@ -4,10 +4,25 @@ import {setupServer} from 'msw/node';
 import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import RnR from './index.jsx';
+import ReviewList from './RnRComponents/ReviewList.jsx';
+import RatingsBreakdown from './RnRComponents/RatingsBreakdown.jsx';
+import CharacteristicsBreakdown from './RnRComponents/CharacteristicsBreakdown.jsx';
 
 describe('tests for rendering component', () => {
   test('tests rendering of RnR component', () => {
     render(<RnR />);
-    expect(screen.getByText(/This is Ratings and Reviews!/i)).toBeInTheDocument();
+    expect(screen.getByText(/This is the index file!/i)).toBeInTheDocument();
+  })
+  test('tests rendering of RnR component', () => {
+    render(<ReviewList />);
+    expect(screen.getByText(/This is the review list!/i)).toBeInTheDocument();
+  })
+  test('tests rendering of RnR component', () => {
+    render(<RatingsBreakdown />);
+    expect(screen.getByText(/This is the ratings breakdown!/i)).toBeInTheDocument();
+  })
+  test('tests rendering of RnR component', () => {
+    render(<CharacteristicsBreakdown />);
+    expect(screen.getByText(/This is the characteristics breakdown!/i)).toBeInTheDocument();
   })
 })

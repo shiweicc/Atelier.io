c5de9d50d9b1f00f0155d650d94c558757006bf4
davidlim7223 <david98@vt.edu>
2022-07-02 16:44:12 -0400

    added authenticatation on server side

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
+const authedGet = require('./helper.js').authedGet;
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
@@ -9,4 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`listening at port ${port}`)
+  authedGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/71697', (err, data) =>{
+    console.log(data);
+  });
 })

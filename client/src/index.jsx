8251025c0eba715ec7fa6389e78efc49be7e81db
Minwen Liu <lynnliu0429@gmail.com>
2022-07-06 15:06:31 -0400

    ignore bundle.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,37 +4,13 @@ import ProductOverview from './components/ProductOverview/index.jsx';
 import RelatedProducts from './components/RelatedProducts/index.jsx';
 import QnA from './components/QnA/index.jsx';
 import RnR from './components/RnR/index.jsx';
-import $ from 'jquery';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      curProductID: 71697,
-      outfitCollection: [],
     }
-    this.getProduct = this.getProduct.bind(this);
-  }
-
-  getProduct () {
-    $.ajax({
-      url: '/products',
-      method: 'GET',
-      contentType: 'application/json',
-      success: (res) => {
-        console.log('Successful get request!');
-        console.log(res);
-      },
-      error: (err) => {
-        console.log('Unsuccessful get request.');
-        console.log(err);
-      }
-    })
-  }
-
-  componentDidMount () {
-    this.getProduct();
   }
 
   render() {
@@ -42,9 +18,9 @@ class App extends React.Component {
     <div>
       React is working!
       <ProductOverview />
-      <RelatedProducts curProductID={this.state.curProductID}/>
+        <RelatedProducts />
       <QnA />
-      {/* <RnR /> */}
+        {/* <RnR /> */}
     </div>
     )
   }

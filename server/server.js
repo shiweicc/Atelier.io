26d23448e2ed3445b398537981089c5ad73ad5d7
Shiwei Chen <shiweichenus@gmail.com>
2022-07-27 16:02:54 -0500

    Update port in server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const authedGet = require('./helper.js').authedGet;
 const authedPost = require('./helper.js').authedPost;
 const authedPut = require('./helper.js').authedPut;
